f11e32df388f1f5ce47bb45e8e2ee3c596a224e2
Dmitry Sidorov <demsys14@gmail.com>
2022-01-09 18:19:26 +0300

    PipeEx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PipeExamples';
+  birthday = new Date(1988, 3, 15);
+  cost = 1.234;
+  power = 5;
+  factor = 1;
+
+  toggle = true; // start with true == shortDate
+  get format() { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
+
+  message$ = new Observable<string>();
+
+  constructor() {
+    this.message$ = this.getResendObservable();
+  }
+
+  private messages = [
+    'You are my hero!',
+    'You are the best hero!',
+    'Will you be my hero?'
+  ];
+
+  resend() {
+    this.message$ = this.getResendObservable();
+  }
+
+  private getResendObservable() {
+    return interval(2000).pipe(
+      map(i => this.messages[i]),
+      take(this.messages.length)
+    );
+  }
 }
---
+++ src/app/exp-strength.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ExpStrengthPipe } from './exp-strength.pipe';
+
+describe('ExpStrengthPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ExpStrengthPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
